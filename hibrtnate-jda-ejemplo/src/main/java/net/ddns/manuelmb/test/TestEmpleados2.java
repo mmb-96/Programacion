a3dd7f6ca053a0d489707da664ca32c7bfb2335b
Manuel Melero <44204425+mmb-96@users.noreply.github.com>
2019-02-24 14:54:48 +0100

    JDA-Hibernate Auto-Aprendizaje Parte 8

--- hibrtnate-jda-ejemplo/src/main/java/net/ddns/manuelmb/test/TestEmpleados2.java
+++ hibrtnate-jda-ejemplo/src/main/java/net/ddns/manuelmb/test/TestEmpleados2.java
@@ -1,5 +1,6 @@
 /**
- * Entidad administrada global.
+ * Ralizar que una entidad no administrada, podamos realizar modificaciones y borrado de ella.
+ * Volviendo a poner administrada.
  */
 package net.ddns.manuelmb.test;
 
@@ -12,44 +13,62 @@ import javax.persistence.Persistence;
 
 import net.ddns.manuelmb.modelo.Empleado;
 
-public class TestEmpleado {
+public class TestEmpleados2 {
 	
-	private static EntityManager manager;
 	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("aplicacion");
 	
 	public static void main(String[] args) {
-		manager = emf.createEntityManager();
 		
 		insertInicial();
-		imprimirTodo();
-		
-		manager.getTransaction().begin();
-		Empleado e = manager.find(Empleado.class, 25L);
-		e.setNombre("David");
-		e.setApellidos("Mulero");
-		manager.getTransaction().commit();
-		imprimirTodo();
-		
-		manager.close();
-	}
-
-	private static void insertInicial() {
-		Empleado a = new Empleado(10L, "Melero", "Manuel", new GregorianCalendar(1996, 2, 6).getTime());
+	
+		EntityManager manager = emf.createEntityManager();
 		Empleado b = new Empleado(11L, "Cruz", "José", new GregorianCalendar(2000, 6, 12).getTime());
 		
 		manager.getTransaction().begin();
-		manager.persist(a);
 		manager.persist(b);
 		manager.getTransaction().commit();
+		manager.close();
+		
+		imprimirTodo();
+		
+		manager = emf.createEntityManager();
+		manager.getTransaction().begin();
+		b.setNombre("Dani");
+		manager.merge(b);
+		manager.getTransaction().commit();
+		manager.close();
+		
+		imprimirTodo();
+		
+		manager = emf.createEntityManager();
+		manager.getTransaction().begin();
+		b = manager.merge(b);
+		manager.remove(b);
+		manager.getTransaction().commit();
+		manager.close();
+		
+		imprimirTodo();
+	}
+
+	private static void insertInicial() {
+		EntityManager manager = emf.createEntityManager();
+		Empleado a = new Empleado(10L, "Melero", "Manuel", new GregorianCalendar(1996, 2, 6).getTime());
+		
+		manager.getTransaction().begin();
+		manager.persist(a);
+		manager.getTransaction().commit();
+		manager.close();
 	}
 	
 	@SuppressWarnings("unchecked")
 	private static void imprimirTodo() {
+		EntityManager manager = emf.createEntityManager();
 		List<Empleado> empleados =(List<Empleado>) manager.createQuery("FROM Empleado").getResultList();
 		System.out.println("En la base de datos hay " + empleados.size()+ " empleados");
 		for (Empleado emp : empleados) {
 			System.out.println(emp.toString());
 		}
+		manager.close();
 		
 	}
 }
