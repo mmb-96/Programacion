02aa9a0a715d29d3d3e2c73d62d7956ac0af3f01
Manuel Melero <44204425+mmb-96@users.noreply.github.com>
2019-02-24 14:33:34 +0100

    Revert "Preuba Manuel 2102"
    This reverts commit 75f06869ef391b1f000268904457d4f46ebe16d1.

--- Manuel2102/src/Categoria.java
+++
@@ -1,8 +0,0 @@
-/**
- * Ejercicio Torneo Ajedrez.
- * @author Manuel Melero
- * Estas categorias seran utilizadas para la case Arbitro.
- */
-public enum Categoria {
-	Principal, Adjunto, Auxiliar
-}
--- Manuel2102/src/Ficha.java
+++
@@ -1,9 +0,0 @@
-/**
- * Ejercicio Torneo Ajedrez.
- * @author Manuel Melero
- *
- */
-
-public enum Ficha {
-	B,V
-}
